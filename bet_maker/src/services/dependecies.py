d4cf918ff306468a96468a76ee845e07a4362fd6
Дмитрий <goloslana245@gmail.com>
2025-04-04 02:44:31 +0300

    rabbitmq

--- bet_maker/src/services/dependecies.py
+++ bet_maker/src/services/dependecies.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Session
 from database.dependencies import get_db_session
 
 
-
-def get_service(service_cls, database_session: Session = Depends(get_db_session)):
-    def dependency():
+def get_service(service_cls):
+    def dependency(database_session: Session = Depends(get_db_session)):
         return service_cls(database_session)
-    return dependency()
+
+    return dependency

0a0aebbe90f2ae770c8fdc602849d97ecff03fc5
Дмитрий <goloslana245@gmail.com>
2025-04-04 02:28:46 +0300

    rabbitmq

--- line_provider/src/repository/dependecies.py
+++
@@ -1,6 +0,0 @@
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-
-async def get_repository(db: AsyncSession = Depends(get_db)) -> MyRepository:
-    return MyRepository(db)

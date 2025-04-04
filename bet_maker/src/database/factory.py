21b2a36888f81ae44b9d961c332a4c6fe440f6f1
Дмитрий <goloslana245@gmail.com>
2025-04-04 21:14:11 +0300

    cache

--- bet_maker/src/database/factory.py
+++ bet_maker/src/database/factory.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-from sqlalchemy import AsyncAdaptedQueuePool
 from sqlalchemy.ext.asyncio import (
-    AsyncEngine,
-    AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
@@ -10,27 +7,14 @@ from sqlalchemy.ext.asyncio import (
 from src.database.settings import postgres_settings
 
 
-async def setup_db(app: FastAPI) -> None:
+async def start_database_connection():
     engine = create_async_engine(str(postgres_settings.postgresql_settings.db_url))
     session_factory = async_sessionmaker(
         engine,
         expire_on_commit=False,
     )
-    app.state.db_engine = engine
-    app.state.db_session_factory = session_factory
+    return engine, session_factory
 
 
-def create_session_maker(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
-    pool: async_sessionmaker[AsyncSession] = async_sessionmaker(
-        bind=engine, expire_on_commit=False
-    )
-    return pool
-
-
-def create_engine(database_url: str) -> AsyncEngine:
-    return create_async_engine(
-        url=database_url,
-        poolclass=AsyncAdaptedQueuePool,
-        pool_size=3,
-        max_overflow=2
-    )
+async def setup_db(app: FastAPI) -> None:
+    app.state.db_engine, app.state.db_session_factory = await start_database_connection()
--- bet_maker/src/messaging/main.py
+++ bet_maker/src/messaging/main.py
@@ -1,18 +1,29 @@
 import asyncio
+from functools import partial
 
 from caching.client import RedisClient
+from database.dependencies import get_db_session
+from database.factory import start_database_connection
 from messaging.client import RabbitMQClient
 from messaging.handlers import process_created_events, process_updated_events
+from repository.bet import BetRepository
+from repository.event import EventRepository
 
 
 async def main():
     rabbitmq_client = RabbitMQClient()
+
     redis_client = RedisClient()
     await redis_client.connect()
 
+    engine, session_factory = start_database_connection()
+
     await rabbitmq_client.connect()
     await rabbitmq_client.declare_queues()
-    await rabbitmq_client.consume(process_created_events, process_updated_events)
+    await rabbitmq_client.consume(
+        partial(process_created_events, EventRepository(redis_client)),
+        partial(process_updated_events, BetRepository(next(get_db_session(session_factory))))
+    )
     print(" [*] Waiting for messages. To exit press CTRL+C")
     await asyncio.Future()  # Run forever
 

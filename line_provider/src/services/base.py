6b0f1c7a92b5a80c1ed39e526e7b8bb3840bf3d3
Дмитрий <goloslana245@gmail.com>
2025-04-04 12:36:12 +0300

    refactor

--- line_provider/src/services/base.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from repository.base import BaseRepository
-
-
-class BaseService:
-    def __init__(self, repository_cls: type[BaseRepository], session: AsyncSession):
-        self.repository = repository_cls(session)
--- line_provider/src/services/event.py
+++ line_provider/src/services/event.py
@@ -5,15 +5,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.models.event import Event
 from src.enums.event import EventState
 from src.repository.event import EventRepository
-from src.repository.event_sender import EventsenderRepository
-from src.schemas.event import EventCreate, EventResponse
-from src.services.base import BaseService
+from src.schemas.event import EventCreate
 
 
-class EventService(BaseService):
-    def __init__(self, session: AsyncSession, event_sender: EventsenderRepository):
-        super().__init__(EventRepository, session)
+class EventService:
+
+    def __init__(self, session: AsyncSession, event_sender):
         self.event_sender = event_sender
+        self.repository = EventRepository(session)
 
     async def create_event(self, event_data: EventCreate) -> Event | None:
         created_event = await self.repository.create(
@@ -25,7 +24,7 @@ class EventService(BaseService):
     async def get_event_by_id(self, event_id: int) -> Event | None:
         return await self.repository.get_by_id(event_id)
 
-    async def get_all_events(self) -> Sequence[EventResponse]:
+    async def get_all_events(self) -> Sequence[Event]:
         return await self.repository.get_all()
 
     async def update_event_status(self, event_id: int, new_status: EventState) -> Event | None:

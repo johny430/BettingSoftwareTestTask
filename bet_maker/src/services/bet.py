cd1095bbef44d17f8cea848fc10180f6e2590a10
Дмитрий <goloslana245@gmail.com>
2025-04-05 13:52:43 +0300

    cache

--- bet_maker/src/services/bet.py
+++ bet_maker/src/services/bet.py
@@ -3,6 +3,7 @@ from typing import Sequence
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from bet_maker.src.repository.bet import BetRepository
+from database.models.bet import Bet
 from schemas.bet import BetResponse
 
 
@@ -11,12 +12,8 @@ class BetService:
     def __init__(self, session: AsyncSession):
         self.repository = BetRepository(session)
 
-    async def get_all_bets(self) -> Sequence[BetResponse]:
-        all_bets = await self.repository.get_all_bets()
-        return [
-            BetResponse(id=bet.id, state=bet.state, sum=bet.sum, event_id=bet.event_id)
-            for bet in all_bets
-        ]
+    async def get_all_bets(self) -> Sequence[Bet]:
+        return await self.repository.get_all_bets()
 
-    async def create_bet(self, created_bet) -> int | None:
-        return await self.repository.create_bet(created_bet)
+    async def create_bet(self, bet) -> int | None:
+        return await self.repository.create_bet(bet)

93c67a961b52382f0738a05aec52f669b9ba4025
Дмитрий <goloslana245@gmail.com>
2025-04-05 16:12:21 +0300

    bug fix

--- bet_maker/src/messaging/main.py
+++ bet_maker/src/messaging/main.py
@@ -1,24 +1,23 @@
 from messaging.client import RabbitMQClient
 from messaging.handlers import process_updated_events, process_created_events
 
+
+async def main():
+    client = RabbitMQClient()
+    await client.connect()
+    await client.declare_queues()
+    await client.consume(process_created_events, process_updated_events)
+
+    print("Waiting for messages")
+    try:
+        await asyncio.Future()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await client.close()
+
+
 if __name__ == '__main__':
     import asyncio
-
-
-    async def main():
-
-        client = RabbitMQClient()
-        await client.connect()
-        await client.declare_queues()
-        await client.consume(process_created_events, process_updated_events)
-
-        print("Waiting for messages")
-        try:
-            await asyncio.Future()
-        except asyncio.CancelledError:
-            pass
-        finally:
-            await client.close()
-
 
     asyncio.run(main())

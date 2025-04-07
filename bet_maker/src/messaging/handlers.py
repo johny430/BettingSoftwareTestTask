ac12057658d9ccfc23880b79466b3819d1a0c171
Дмитрий <goloslana245@gmail.com>
2025-04-07 16:34:14 +0300

    refactor

--- bet_maker/src/messaging/handlers.py
+++ bet_maker/src/messaging/handlers.py
@@ -11,26 +11,21 @@ from src.services.event import EventService
 
 logger = logging.getLogger(__name__)
 
+
 async def process_created_events(message: IncomingMessage, event_service: EventService):
     async with message.process():
-        body = json.loads(message.body.decode())
-        await event_service.add_event(EventCreate(
-            id=body['event_id'],
-            coefficient=body['coefficient'],
-            deadline=int(body['deadline']),
-            status=body['state']
-        ))
+        await event_service.add_event(EventCreate(**json.loads(message.body.decode())))
 
 
 async def process_updated_events(message: IncomingMessage, bet_service: BetService):
     async with message.process():
         body = json.loads(message.body.decode())
-        if 'event_id' not in body or 'state' not in body:
+        if 'event_id' not in body or 'status' not in body:
             logger.error("asdasdasd")
             return
-        logger.error(f"new_st: {get_bet_status_based_on_event_status(EventStatus(body['state']))}")
+        logger.error(f"new_st: {get_bet_status_based_on_event_status(EventStatus(body['status']))}")
         logger.error(f"event_id: {body['event_id']}")
         await bet_service.update_status_by_event_id(
             body['event_id'],
-            get_bet_status_based_on_event_status(EventStatus(body['state']))
+            get_bet_status_based_on_event_status(EventStatus(body['status']))
         )
